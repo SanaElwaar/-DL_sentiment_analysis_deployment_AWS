6976cae4f320487c855b88008b7aea6477cffde0
Sana Elwaar <sana.elwaar@gmail.com>
2021-10-06 12:36:27 +0200

    update predict_fn - correct review length
    stop at 500 max

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -72,7 +72,8 @@ def predict_fn(input_data, model):
 
     #data_X , data_len = convert_and_pad_data(model.word_dict, input_data)
     data_X , data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
-
+    data_X= data_X[:500]
+    
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
